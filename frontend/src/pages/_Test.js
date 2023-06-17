e6b5fcb035b7197af44409ceb41414d7f21c77f3
Woohyun Kim <woodenkim92@gmail.com>
2023-06-16 22:54:55 -0500

    Test Category Dropdowns

--- frontend/src/pages/_Test.js
+++ frontend/src/pages/_Test.js
@@ -1,21 +1,25 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import { useParams, Link, useNavigate} from 'react-router-dom'
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 import { ReactComponent as SaveIcon } from '../assets/save.svg'
+import Test from "./Test";
+import Dropdown from 'react-bootstrap/Dropdown'
 
 // let dummyData = [{"id":"1", "body":"Get milk" }, {"id":"2", "body":"Wash car" }, {"id":"3", "body":"Start coding"}]
-function TestFunction() {
+export default function TestFunction() {
     let params = useParams()
     let navigate = useNavigate()
     let testId2 = params.id
+
+
 
     // let testItem = dummyData.find((test) => test.id === testId)
 
     let [test2, setTest2] = useState(null)
 
-    useEffect(() => {
-        if(testId2 !== 'add') getTest()
-        }, [testId2])
+        useEffect(() => {
+            if(testId2 !== 'add') getTest()
+            }, [testId2])
 
 
     let getTest = async () => {
@@ -41,14 +45,14 @@ function TestFunction() {
             headers: {
                 'Content-Type': 'application/json'
             }, body: JSON.stringify({
-                "category": test.category,
-                "vac_type": test.vac_type,
-                "test_status": test.test_status,
-                "assigned1": test.assigned1,
-                "assigned2": test.assigned2,
-                "due_date": test.due_date,
-                "complete_date": test.complete_date,
-                "notes": test.notes,
+                "category": test2.category,
+                "vac_type": test2.vac_type,
+                "test_status": test2.test_status,
+                "assigned1": test2.assigned1,
+                "assigned2": test2.assigned2,
+                "due_date": test2.due_date,
+                "complete_date": test2.complete_date,
+                "notes": test2.notes,
 
             })
         })
@@ -65,19 +69,69 @@ function TestFunction() {
         navigate('/_tests')
     }
 
-    return (
-            <div>
+
+    const [setvalue, setValue] = useState("")
+
+    const handleSelect = (e) => {
+        if (e === "") {
+            setValue("")
+        } else {
+            setValue(e)
+        }
+    }
+
+
+    const handleInput = (e) => {
+        e.persist()
+        setTest2({...test2, [e.target.name]: e.target.value});
+    }
+
+    return (  <div>
                 <div className="input-group mb-3">
                     <div className="input-group-prepend">
-                        <span className="input-group-text" id="basic-addon1">@</span>
+                        <span className="input-group-text" id="basic-addon1">TEST ID:</span>
                     </div>
-                    <input type="text" className="form-control" placeholder="Username" aria-label="Username"
-                           aria-describedby="basic-addon1">
-                    </input>
-            </div>
+                    VAC-0001
+                </div>
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <Dropdown onSelect={handleSelect} >
+                          <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            Test Category
+                          </Dropdown.Toggle>
+
+                          <Dropdown.Menu>
+                            <Dropdown.Item eventKey="CR">CR</Dropdown.Item>
+                            <Dropdown.Item eventKey="Field Test">Field Test</Dropdown.Item>
+                            <Dropdown.Item eventKey="Real-Use">Real-Use</Dropdown.Item>
+                            <div role="separator" className="dropdown-divider"></div>
+                            <Dropdown.Item eventKey="">Other</Dropdown.Item>
+                          </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <div>
+
+                        {/*<input value = {setValue()} onChange={handleSelect} />*/}
+                        <input type="text" className="form-control"
+                               aria-label="Text input with dropdown button" aria-describedby="basic-addon1"
+                              value = {setvalue} />
+                               {/*value={test2?.category} onChange={handleInput} name="category"/>*/}
 
 
+
+
+
+                    </div>
+                </div>
+                <p>vacuum type</p>
+                 <p>assign to 1</p>
+                 <p>assign to 2</p>
+                 <p>due date</p>
+
+
+
+
+        </div>
     )
-}
 
-.
+}
